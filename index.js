70d3d19d4c571f4f8f2504c353a21b344600edac
Adrian Stainforth <adrian.stainforth@gmail.com>
2016-10-24 14:18:48 +0100

    fix dependancies

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.check_calls = require('./lib/check_calls');
 exports.check_multiple_calls = require('./lib/check_multiple_calls');
-exports.make_calls= require('./lib/make_calls');
+exports.make_calls = require('./lib/make_calls');
 exports.not_called = require('./lib/not_called');
